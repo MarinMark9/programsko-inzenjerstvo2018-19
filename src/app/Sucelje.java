02786703d4a5f325e12f3c5cf4fa57bfde05f734
Marin Mark <marin.markanjevic@gmail.com>
2019-06-09 17:55:12 +0200

    Odabir parametara u sucelju - fixed

--- src/app/Sucelje.java
+++ src/app/Sucelje.java
@@ -12,7 +12,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
@@ -26,7 +26,8 @@ public class Sucelje extends JFrame {
 	
 	private JPanel contentPane;
 	private JTextField textField;
-
+	String headers = "";
+	
 	public Sucelje() {
 		setTitle("Tra\u017Eilica");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -38,7 +39,7 @@ public class Sucelje extends JFrame {
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel.setBounds(12, 13, 170, 650);
+		panel.setBounds(12, 13, 195, 650);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -57,7 +58,7 @@ public class Sucelje extends JFrame {
 		
 		textField = new JTextField();
 		textField.setColumns(10);
-		textField.setBounds(23, 106, 116, 22);
+		textField.setBounds(23, 106, 125, 22);
 		panel.add(textField);
 		
 		JLabel label_1 = new JLabel("Odaberi parametre:");
@@ -65,33 +66,87 @@ public class Sucelje extends JFrame {
 		panel.add(label_1);
 		
 		JCheckBox checkBox = new JCheckBox("At scene time");
-		checkBox.setBounds(8, 178, 113, 25);
+		checkBox.setBounds(8, 178, 125, 25);
+		/*checkBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(checkBox.isSelected()) {
+					headers += "`Event Clearance Date`, ";
+				} else {
+					headers = headers.replace("`Event Clearance Date`,", "");
+				}
+			}
+		});*/
 		panel.add(checkBox);
 		
 		JCheckBox checkBox_1 = new JCheckBox("Event description");
 		checkBox_1.setBounds(8, 201, 150, 25);
+		/*checkBox_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(checkBox_1.isSelected()) {
+					headers += "`Event Clearance Description`, ";
+				} else {
+					headers = headers.replace("`Event Clearance Description`,", "");
+				}
+			}
+		});*/
 		panel.add(checkBox_1);
 		
 		JCheckBox checkBox_2 = new JCheckBox("Event code");
 		checkBox_2.setBounds(8, 224, 113, 25);
+		/*checkBox_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(checkBox_2.isSelected()) {
+					headers += "`Event Clearance Code`, ";
+				} else {
+					headers = headers.replace("`Event Clearance Code`,", "");
+				}
+			}
+		});*/
 		panel.add(checkBox_2);
 		
 		JCheckBox checkBox_3 = new JCheckBox("Block location");
-		checkBox_3.setBounds(8, 247, 113, 25);
+		checkBox_3.setBounds(8, 247, 125, 25);
+		/*checkBox_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(checkBox_3.isSelected()) {
+					headers += "`Hundred Block Location`, ";
+				} else {
+					headers = headers.replace("`Hundred Block Location`,", "");
+				}
+			}
+		});*/
 		panel.add(checkBox_3);
 		
 		JCheckBox checkBox_4 = new JCheckBox("Initial type desc.");
 		checkBox_4.setBounds(8, 269, 150, 25);
+		/*checkBox_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(checkBox_4.isSelected()) {
+					headers += "`Initial Type Description`, ";
+				} else {
+					headers = headers.replace("`Initial Type Description`,", "");
+				}
+			}
+		});*/
 		panel.add(checkBox_4);
 		
 		JCheckBox checkBox_5 = new JCheckBox("Zone");
 		checkBox_5.setBounds(8, 290, 113, 25);
+		/*checkBox_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(checkBox_5.isSelected()) {
+					headers += "`Zone/Beat`, ";
+				} else {
+					headers = headers.replace("`Zone/Beat `,", "");
+				}
+			}
+		});*/
 		panel.add(checkBox_5);
 		
 		JButton button = new JButton("Odjava");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				JOptionPane.showMessageDialog(null, "Uspješno ste odjavljeni!");
+				JOptionPane.showMessageDialog(null, "Uspjeï¿½no ste odjavljeni!");
 				dispose();
 				new Prijava();
 			}
@@ -103,7 +158,7 @@ public class Sucelje extends JFrame {
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_1.setBounds(184, 13, 995, 650);
+		panel_1.setBounds(209, 13, 995, 650);
 		panel_1.add(txt);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
@@ -113,29 +168,83 @@ public class Sucelje extends JFrame {
 		sp.setSize(975, 635);
 		panel_1.add(sp);
 		this.setVisible(true);
+		
+		JLabel lblNeispravanthr = new JLabel("");
+		
 		btnTrai.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 		        long start = System.currentTimeMillis();
-				int threshold = 1000, cnt = 0;
-				
+		        int threshold = 1000, cnt = 0;
+		      
+	        	lblNeispravanthr.setBounds(15, 130, 150, 15);
+	        	lblNeispravanthr.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		        try {
+		        	 threshold = Integer.parseInt(textField.getText());
+		        	 lblNeispravanthr.setText(" ");
+		        	 lblNeispravanthr.setForeground(new Color(34, 34, 178));
+			         lblNeispravanthr.setText("U redu");
+		        } catch(Exception exc) {
+		        	lblNeispravanthr.setText(" ");
+		        	lblNeispravanthr.setForeground(new Color(178, 34, 34));
+		        	lblNeispravanthr.setText("PogreÅ¡an unos. Unesite broj");
+		        }
+		        panel.add(lblNeispravanthr);
+		        ifChecked();
 				/*String query = ("select `Zone/Beat`, `Event Clearance Group`, `Event Clearance Date` from (select distinct `Event Clearance Code`, `Event Clearance Date`, `Hundred Block Location`, `Zone/Beat` from "
 						+ "(select distinct `Event Clearance Code`, `Event Clearance Date`, `Hundred Block Location` from zlocin) as b "
 						+ "natural join (select distinct `Hundred Block Location`, `Zone/Beat` from zona) as c) as a "
 						+ "natural join (select distinct `Event Clearance Code`, `Event Clearance Group` from grupa) as d");
 		        String[] header = {"Zone/Beat", "Event Clearance Group", "Event Clearance Date"};*/
-				
+		        String querry = "";
+		        querry = "SELECT " + headers + "FROM zlocin";
+		        querry = querry.replace("  ", " ").replace(", FROM", " FROM");
 				String query = "select `Event Clearance Description`, `Event Clearance Date`, `Zone/Beat` from zlocin";
-				String[] header = {"Event Clearance Description", "Event Clearance Date", "Zone/Beat"};
-				
-		        ArrayList<Rule> resultRules = new FPGrowth(query, threshold, header).returnResult();
-		        
+				String[] header = headers.replace("`","").split(", ");/*{"Event Clearance Description", "Event Clearance Date", "Zone/Beat"};*/
+				System.out.println(querry);
+				System.out.println(headers);
+		        ArrayList<Rule> resultRules = new FPGrowth(querry, threshold, header).returnResult();
+		        txt.setText("");
 		        for (Rule s : resultRules) {
 		        	cnt++;
 		        	txt.append("Rule #" + cnt + '\n' + s.toString() + '\n');
 		        }
+		        headers = "";
 		        System.out.println("Printed lines: " + cnt);
 		        System.out.println("Ending time: " + (System.currentTimeMillis() - start));
 			}
+			public void ifChecked() {
+				if(checkBox.isSelected()) {
+					headers += "`Event Clearance Date`, ";
+				} else {
+					headers = headers.replace("`Event Clearance Date`,", "");
+				}
+				if(checkBox_1.isSelected()) {
+					headers += "`Event Clearance Description`, ";
+				} else {
+					headers = headers.replace("`Event Clearance Description`,", "");
+				}
+				if(checkBox_2.isSelected()) {
+					headers += "`Event Clearance Code`, ";
+				} else {
+					headers = headers.replace("`Event Clearance Code`,", "");
+				}
+				if(checkBox_3.isSelected()) {
+					headers += "`Hundred Block Location`, ";
+				} else {
+					headers = headers.replace("`Hundred Block Location`,", "");
+				}
+				if(checkBox_4.isSelected()) {
+					headers += "`Initial Type Description`, ";
+				} else {
+					headers = headers.replace("`Initial Type Description`,", "");
+				}
+				if(checkBox_5.isSelected()) {
+					headers += "`Zone/Beat`, ";
+				} else {
+					headers = headers.replace("`Zone/Beat `,", "");
+				}
+			}
 		}); 
 	}
+	
 }
